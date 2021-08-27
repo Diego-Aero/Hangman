b0189ffb9c33e343502eed96b8da19508a25e5e6
Diego-Aero <85786727+Diego-Aero@users.noreply.github.com>
2021-08-27 18:25:18 +0200

    Definimos getGuessedWord
    Esta función devuelve la palabra a adivinar en el formato popular formado por las letras ya descubiertas y guiones bajos para las que faltan por averiguar. Es importante que el guión bajo vaya acompañado de un espacio detrás para que no se superpongan si hay más de uno junto.

--- hangman.py
+++ hangman.py
@@ -64,7 +64,13 @@ def getGuessedWord(secretWord, lettersGuessed):
     returns: string, comprised of letters and underscores that represents
       what letters in secretWord have been guessed so far.
     '''
-    pass
+    word=''
+    for c in secretWord:
+        if c not in lettersGuessed:
+            word+='_ '
+        else:
+            word+=c
+    return word
 
 
 
